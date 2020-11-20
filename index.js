9eab53cfd8e1e64dd6ca81ef4123cb2995fdd297
roroyu <yuroro@msn.com>
2020-11-20 02:35:04 +0000

    stitching test

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const graphqlHTTP = require('express-graphql').graphqlHTTP;
 // import { createSchema, CallBackendArguments } from 'swagger-to-graphql';
 const { createSchema, CallBackendArguments }  = require( 'swagger-to-graphql');
 
+const { stitchSchemas }  = require('@graphql-tools/stitch');
+const { mergeSchemas }  = require('@graphql-tools/merge');
+const { makeExecutableSchema, delegateToSchema } = require('graphql-tools')
+
 const fetch = require("node-fetch");
 
 // Define your own http client here
@@ -36,6 +40,7 @@ async function callBackend({
   });
 
   const text = await response.text();
+  console.log(text)
   if (200 <= response.status && response.status < 300) {
     try {
       return JSON.parse(text);
@@ -53,19 +58,61 @@ createSchema({
   callBackend,
 })
   .then(schema => {
-    console.log(schema)
+    // console.log(schema)
+    // const extendedSchema = `
+    //     extend type Poem {
+    //       author_info: [Author!]!
+    //     }
+    // `;
+    const gatewaySchema = stitchSchemas({
+      // subschemas: [ schema, extendedSchema ],
+      subschemas: [ schema ],
+      // mergeTypes: true, // << optional in v7
+      typeDefs: `
+        extend type Poem {
+          author_info: [Author]
+        }
+      `,
+      resolvers: {
+        Poem: {  
+          author_info: {
+            fragment: `... on Poem { author }`,
+            resolve: (parent, args, context, info) => {
+                console.log('parent: ', parent.author)
+                console.log('args: ', args)
+                // console.log('context: ', context)
+                // console.log('info: ', info.schema)
+                const author_like = parent.author;
+                // return info.mergeInfo.delegateToSchema({
+                return delegateToSchema({
+                    schema: info.schema,
+                    // operation: 'getAuthor',
+                    operation: 'query',
+                    // fieldName: 'author_like',
+                    fieldName: 'getAuthor',
+                    args: { author_like },
+                    context,
+                    info
+                })
+            }
+          }, 
+        },
+      }},
+    )
     app.use(
       '/graphql',
       graphqlHTTP(() => {
         return {
-          schema,
+          // schema,
+          schema: gatewaySchema,
+          // schema1,
           graphiql: true,
         };
       }),
     );
- 
-    app.listen(3009, 'localhost', () => {
-      console.info('http://localhost:3009/graphql');
+
+    app.listen(5003, '0.0.0.0', () => {
+      console.info('http://localhost:5003/graphql');
     });
   })
   .catch(e => {
